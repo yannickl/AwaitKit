fb8fffbdd707f2bd4fdc5f61f1809d78fd5e247e
Yannick Loriot <yannick.loriot@gmail.com>
2016-05-16 20:01:52 +0200

    fixing typos

--- Example/AwaitedPromiseKitTests/AwaitedPromiseKitTests.swift
+++ Example/AwaitedPromiseKitTests/AwaitedPromiseKitTests.swift
@@ -1,17 +1,35 @@
-//
-//  AwaitedPromiseKitTests.swift
-//  AwaitedPromiseKitTests
-//
-//  Created by Yannick LORIOT on 16/05/16.
-//  Copyright © 2016 Yannick Loriot. All rights reserved.
-//
+/*
+ * AwaitedPromiseKit
+ *
+ * Copyright 2016-present Yannick Loriot.
+ * http://yannickloriot.com
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ *
+ */
 
 import PromiseKit
 import XCTest
 @testable import AwaitedPromiseKit
 
 class AwaitedPromiseKitTests: XCTestCase {
-  func testSimpleAsyncBlock() {
+  func testSimpleValidAsyncBlock() {
     let expectation = expectationWithDescription("Async should return value")
 
     let promise: Promise<String> = async {
@@ -29,7 +47,7 @@ class AwaitedPromiseKitTests: XCTestCase {
     }
   }
 
-  func testSimpleDelayedAsyncBlock() {
+  func testSimpleDelayedValidAsyncBlock() {
     let expectation = expectationWithDescription("Async should return value")
 
     let promise: Promise<String> = async {
@@ -45,6 +63,24 @@ class AwaitedPromiseKitTests: XCTestCase {
     waitForExpectationsWithTimeout(0.5) { error in
       if error == nil {
         XCTAssertEqual(promise.value, "AwaitedPromiseKit")
+      }
+    }
+  }
+
+  func testSimpleInvalidAsyncBlock() {
+    let expectation = expectationWithDescription("Async should not return value")
+
+    let promise: Promise<String> = async {
+      throw NSError(domain: "com.yannickloriot.error", code: 0, userInfo: nil)
+    }
+
+    promise.error { err in
+      expectation.fulfill()
+    }
+
+    waitForExpectationsWithTimeout(0.1) { error in
+      if error == nil {
+        XCTAssertNil(promise.value)
       }
     }
   }
